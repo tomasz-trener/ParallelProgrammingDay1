e54c862f6f95b1ab74eb92f41350ddfa08fdfe6f
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-21 08:49:39 +0200

    q

--- P02CancellingTasks/Program.cs
+++ P02CancellingTasks/Program.cs
@@ -26,7 +26,7 @@
 
 class Example2
 {
-    public static void Main(string[] args)
+    public static void Main2(string[] args)
     {
         var cts = new CancellationTokenSource();
         var token = cts.Token;
@@ -54,6 +54,79 @@ class Example2
         Console.WriteLine("Press any key to cancel the task");
         Console.ReadKey();
     }
+    //https://github.com/tomasz-trener/ParallelProgrammingDay1
 
+}
+
+class Example3
+{
+    public static void Main3(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+
+        var t = new Task(() =>
+        {
+            int i = 0;
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+                //if (token.IsCancellationRequested)
+                //{
+                //    throw new OperationCanceledException(token);
+                //}
+                Console.WriteLine($"{i++}\t");
+            }
+        }, token);
+
+        t.Start();
+
+        Console.ReadKey();
+        cts.Cancel();
+
+        Console.WriteLine("Press any key to cancel the task");
+        Console.ReadKey();
+    }
+    //https://github.com/tomasz-trener/ParallelProgrammingDay1
+
+}
+
+class Example4
+{
+    public static void Main(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+        token.Register(() =>
+        {
+            Console.WriteLine("Cancellation requested");
+          //  token.ThrowIfCancellationRequested(); // unikamy takiego podejscia
+        });
+
+        var t = new Task(() =>
+        {
+            int i = 0;
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+                //if (token.IsCancellationRequested)
+                //{
+                //    throw new OperationCanceledException(token);
+                //}
+                Console.WriteLine($"{i++}\t");
+            }
+        }, token);
+
+        t.Start();
+
+        Console.ReadKey();
+        cts.Cancel();
+
+        Console.WriteLine("Press any key to cancel the task");
+        Console.ReadKey();
+    }
+    //https://github.com/tomasz-trener/ParallelProgrammingDay1
 
 }
